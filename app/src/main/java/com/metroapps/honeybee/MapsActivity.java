4316f2bf34b81eef26e54b72bbc00d65ddba4655
Thushan <kalharawithanage98@gmail.com>
2020-08-17 20:19:34 +0530

    Added user location tracking

---
+++ app/src/main/java/com/metroapps/honeybee/MapsActivity.java
@@ -0,0 +1,101 @@
+package com.metroapps.honeybee;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    FusedLocationProviderClient client;
+    SupportMapFragment supportMapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_maps );
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById( R.id.map );
+        mapFragment.getMapAsync( this );
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById( R.id.map );
+
+        client = LocationServices.getFusedLocationProviderClient( this );
+
+        if (ActivityCompat.checkSelfPermission( MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions( MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44 );
+        }
+    }
+
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                if(location!=null)
+                {
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap)
+                        {
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("You are Here!");
+                            options.icon( BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                            mMap.addMarker(options);
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng col08 = new LatLng( 6.9122, 79.8829 );
+        mMap.addMarker( new MarkerOptions().position( col08 ).title( "Restaurant" ) );
+        mMap.moveCamera( CameraUpdateFactory.newLatLng( col08 ) );
+
+        getCurrentLocation();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        /*if(requestCode == 44)
+        {*/
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                getCurrentLocation();
+            }
+        //}
+    }
+}
