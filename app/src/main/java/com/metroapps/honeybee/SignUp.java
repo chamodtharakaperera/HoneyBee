2a603ec55360caeb0e45b13250ea54fb3e3fad9d
pererachamod <rpctharakaperera@gmail.com>
2020-08-19 13:21:26 +0530

    Final MAD Update

--- app/src/main/java/com/metroapps/honeybee/SignUp.java
+++ app/src/main/java/com/metroapps/honeybee/SignUp.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import java.sql.Statement;
 public class SignUp extends AppCompatActivity {
 
     private TextView em, no, pw, cpw, reg;
+    private Button signup;
 
     final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
@@ -36,55 +38,8 @@ public class SignUp extends AppCompatActivity {
         pw = findViewById(R.id.txtUName4);
         cpw = findViewById(R.id.txtUName5);
         reg = findViewById(R.id.txtsignuplink2);
+        signup = findViewById(R.id.button3);
 
-
-        if (em.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Enter Email-address", Toast.LENGTH_SHORT).show();
-        } else if (no.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Enter Mobile no", Toast.LENGTH_SHORT).show();
-        } else if (pw.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
-        } else if (cpw.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Re-enter Password", Toast.LENGTH_SHORT).show();
-        } else if (!((pw.getText().toString()).equals(cpw.getText().toString()))) {
-            Toast.makeText(getApplicationContext(), "Passwords mismatch", Toast.LENGTH_SHORT).show();
-        } else {
-            try {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-
-                int k = 0;
-                String username = em.getText().toString();
-
-                Class.forName("net.sourceforge.jtds.jdbc.Driver");
-                connectionURL = "jdbc:jtds:sqlserver://nibm.database.windows.net:1433;DatabaseName=pepperdb;user=adminuser@nibm;password=pass@123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-                con = DriverManager.getConnection(connectionURL);
-
-                String query = "select * from SingUp where uemail = '" + username + "' ";
-
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(query);
-                while (rs.next()) {
-                    k = 1;
-                }
-
-                if (k == 1) {
-                    Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
-                } else if (k == 0) {
-                    String query1 = "insert into Singup(uemail, upassword, mobileno) values('" + username + "','" + pw.getText().toString() + "','" + no.getText().toString() + "')";
-                    Statement st2 = con.createStatement();
-                    st2.executeUpdate(query1);
-                    con.close();
-
-                    Intent bl = new Intent(getApplicationContext(), SignIn.class);
-                    startActivity(bl);
-                }
-            } catch (SQLException e1) {
-                Log.e("MYAPP", "exception", e1);
-            } catch (Exception e) {
-                Log.e("MYAPP", "exception", e);
-            }
-        }
 
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,6 +48,64 @@ public class SignUp extends AppCompatActivity {
                 startActivity(bl2);
             }
         });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (em.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Email-address", Toast.LENGTH_SHORT).show();
+                } else if (no.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Mobile no", Toast.LENGTH_SHORT).show();
+                } else if (pw.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                } else if (cpw.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Re-enter Password", Toast.LENGTH_SHORT).show();
+                } else if (!((pw.getText().toString()).equals(cpw.getText().toString()))) {
+                    Toast.makeText(getApplicationContext(), "Passwords mismatch", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                        StrictMode.setThreadPolicy(policy);
+
+                        int k = 0;
+                        String username = em.getText().toString();
+
+                        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                        connectionURL = "jdbc:jtds:sqlserver://nibm.database.windows.net:1433;DatabaseName=pepperdb;user=adminuser@nibm;password=pass@123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                        con = DriverManager.getConnection(connectionURL);
+
+                        String query2 = "select  * from SingUp where uemail = '" + username + "' ";
+
+                        Statement st2 = con.createStatement();
+                        ResultSet rs2 = st2.executeQuery(query2);
+                        while (rs2.next()) {
+                            k = 1;
+                            //Toast.makeText(getApplicationContext(), "Workiing", Toast.LENGTH_LONG).show();
+                        }
+                        con.close();
+
+                        if (k == 1) {
+                            Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
+                        } else if (k == 0) {
+                            String query3 = "insert into Singup(uemail, upassword, mobileno) values('" + username + "','" + pw.getText().toString() + "','" + no.getText().toString() + "')";
+                            Statement st3 = con.createStatement();
+                            st3.executeUpdate(query3);
+                            con.close();
+
+                            Intent bl = new Intent(getApplicationContext(), SignIn.class);
+                            startActivity(bl);
+                        }
+                    } catch (SQLException e1) {
+                        Log.e("MYAPP", "exception", e1);
+                    } catch (Exception e) {
+                        Log.e("MYAPP", "exception", e);
+                    }
+                }
+            }
+        });
+
 
     }
 
